bf085eb9706d4cadc2adfbe048e9303fe35c1945
Sahil Kumar <sahilkumar012004@gmail.com>
2025-10-11 17:49:25 +0530

    used cors in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { connectDB } from './config/database.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 const app = express()
 app.use(express.json());
 app.use(cookieParser())
+
+app.use(cors())
 
 import authRouter from './routes/auth.js'
 import profileRouter from './routes/profile.js'
