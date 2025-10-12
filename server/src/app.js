b6dc9000641b7e8365fdf10275026e3f61201395
Sahil Kumar <sahilkumar012004@gmail.com>
2025-10-12 23:04:12 +0530

    added cors credentials as true

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,10 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser())
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 import authRouter from './routes/auth.js'
 import profileRouter from './routes/profile.js'
